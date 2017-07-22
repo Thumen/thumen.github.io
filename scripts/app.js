23e9c4efb13daf822c80cb7a7df050eff6f7be32
Thumen <thumen269@gmail.com>
2017-07-22 19:10:30 +0700

    new

--- scripts/app.js
+++ scripts/app.js
@@ -1,32 +1,25 @@
-	var scotchApp = angular.module('scotchApp', ['ngRoute']);
+var scotchApp = angular.module('scotchApp', ['ngRoute']);
 
-	// configure our routes
-	scotchApp.config(function($routeProvider) {
-	    $routeProvider
+// configure our routes
+scotchApp.config(function($routeProvider) {
+    $routeProvider
 
-	    // route for the home page
-	        .when('/', {
-	            templateUrl: 'listpost.html',
-	            controller: 'mainController'
-	        })
-	        .when('/single', {
-	            templateUrl: 'single.html',
-	            controller: 'singleController'
-	        })
-	        .when('/login', {
-	            templateUrl: 'login.html',
-	            controller: 'loginController'
-	        })
-	        .when('/register', {
-	            templateUrl: 'register.html',
-	            controller: 'registerController'
-	        })
+    // route for the home page
+        .when('/', {
+            templateUrl: 'listpost.html',
+            controller: 'mainController'
+        })
+        .when('/single', {
+            templateUrl: 'single.html',
+            controller: 'singleController'
+        })
 
 
-	});
 
-	// create the controller and inject Angular's $scope
-	scotchApp.controller('mainController', function($scope) {
-	    // create a message to display in our view
-	    $scope.message = 'Everyone come and see how good I look!';
-	});
+});
+
+// create the controller and inject Angular's $scope
+scotchApp.controller('mainController', function($scope) {
+    // create a message to display in our view
+    $scope.message = 'Everyone come and see how good I look!';
+});
